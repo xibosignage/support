22217020ed1fd979f916e00e252170715ec4558a
Peter <peter@xibosignage.com>
2020-04-24 12:02:35 +0100

    Add constructor and getErrorData function to AccessDenied Exception

--- src/Xibo/Support/Exception/AccessDeniedException.php
+++ src/Xibo/Support/Exception/AccessDeniedException.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) 2019 Xibo Signage Ltd
+ * Copyright (c) 2020 Xibo Signage Ltd
  */
 
 namespace Xibo\Support\Exception;
@@ -8,8 +8,36 @@ namespace Xibo\Support\Exception;
 
 class AccessDeniedException extends GeneralException
 {
+    protected $property;
+    protected $help;
+
+    /**
+     * AccessDeniedException constructor.
+     * @param string $message
+     * @param string $property
+     * @param string $help
+     */
+    public function __construct($message = '', $property = null, $help = null)
+    {
+        $this->property = $property;
+        $this->help = $help;
+
+        parent::__construct($message, 403, null);
+    }
+
     public function getHttpStatusCode()
     {
         return 403;
+    }
+
+    /**
+     * @return array
+     */
+    protected function getErrorData()
+    {
+        return [
+            'property' => $this->property,
+            'help' => $this->help
+        ];
     }
 }
